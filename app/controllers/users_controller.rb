e811de5cc64d67de06088527b7d849c6d391129c
kent <sample@email.com>
2023-06-20 15:29:07 +0000

    ユーザー検索機能実装完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,11 @@ class UsersController < ApplicationController
   before_action :set_one_month, only: :show
   
   def index
-    @users = User.paginate(page: params[:page])
+    if params[:search].present?
+      @users = User.where("name LIKE ?", "%#{params[:search]}%").paginate(page: params[:page])
+    else
+      @users = User.paginate(page: params[:page])
+    end
   end
     
   def show
